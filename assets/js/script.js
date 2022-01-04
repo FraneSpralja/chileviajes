65c8b02c1d41c05c53e984f60eb254a29a29e9d6
FraneSpralja <spralja.frane@gmail.com>
2022-01-03 21:40:43 -0300

    Add script.js with smooth scroll

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    //Smooth scroll
+    $("a").click(function(event){
+        if (this.hash !==""){
+            event.preventDefault();
+            var smooth = this.hash;
+            $('html, body').animate({
+                scrollTop: $(smooth).offset().top
+            }, 800, function(){
+                window.location.hash = smooth;
+            })
+        }
+    });
